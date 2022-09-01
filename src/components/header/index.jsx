cdedacfd0c3770a18a9d5624bd953bccddbc05f6
aramm2000 <begjanyanaram27@gmail.com>
2022-09-01 15:04:52 +0400

    Added files

---
+++ src/components/header/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './../../assets/fontawesome.css'
+import './style.css'
+
+export default function index() {
+  return (
+    <div>
+      <div className="component_first">
+        <div className="logo_div">
+        <a href="#" className='Logo'><i class="fa-solid fa-car"></i>CARGET</a>
+        </div>
+        <div className="menu">
+            <ul>
+                <li><a href="">Главная</a></li>
+                <li><a href="">О Компании</a></li>
+                <li><a href="">Прокат</a></li>
+                <li><a href="">Тарифы</a></li>
+                <li><a href="">Наш автопарк</a></li>
+                <li><a href="">Новости</a></li>
+                <li><a href="">Контакты</a></li>
+            </ul>
+        </div>
+        
+
+      </div>
+    </div>
+  )
+}
